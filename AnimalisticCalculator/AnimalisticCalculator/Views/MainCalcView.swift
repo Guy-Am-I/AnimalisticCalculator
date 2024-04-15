0db3cba743c12ce8a9e7499b4bdef3338772a653
Guy Steinberg <guysteinberg123@gmail.com>
2024-04-15 12:33:23 -0700

    Added color assets to app. Trying swiftgen to auto generate constants for assets

---
+++ AnimalisticCalculator/AnimalisticCalculator/Views/MainCalcView.swift
@@ -0,0 +1,24 @@
+//
+//  MainCalcView.swift
+//  AnimalisticCalculator
+//
+//  Created by Guy Steinberg on 2024-04-15.
+//
+
+import SwiftUI
+
+struct MainCalcView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainCalcView()
+}
